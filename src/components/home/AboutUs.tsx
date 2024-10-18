6655c7c4dd2ef1d8604dbc30728948b228045493
Karume-lab <danielkarume@outlook.com>
2024-10-18 19:09:57 +0300

    fix: :iphone: fix responsiveness bugs

--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -9,21 +9,21 @@ const AboutUs = () => {
     return (
         <div className='space-y-5 px-5 lg:px-32 py-20'>
 
-            <div className='flex'>
+            <div className='flex flex-col lg:flex-row'>
                 <div>
                     <div className='space-y-10'>
                         <div>
                             <h2 className='text-primary md:text-xl'>ABOUT US</h2>
-                            <h3 className='text-customBlue text-4xl md:text-6xl w-2/5'>We are a team driven by excellence</h3>
+                            <h3 className='text-customBlue text-2xl lg:text-4xl md:text-5xl lg:w-2/5'>We are a team driven by excellence</h3>
                         </div>
                         <div className='space-y-10'>
-                            <p className='w-4/5 text-2xl'>
+                            <p className='lg:w-4/5 text-2xl'>
                                 At Kellian, we are passionate about delivering top-notch auto care services with a focus on quality,
                                 professionalism, and customer satisfaction. Our skilled technicians and advanced tools ensure your vehicle
                                 gets the best treatment, whether it's a simple wash or a complete overhaul.
                             </p>
 
-                            <Button asChild variant={"link"} className='p-0 font-semibold text-black hover:underline'>
+                            <Button asChild variant={"link"} className='p-0 font-semibold text-black hover:underline underline lg:no-underline'>
                                 <Link href={siteConfig.siteLinks.aboutUs}>
                                     Learn more about us
                                 </Link>
--- src/components/home/GetStarted.tsx
+++ src/components/home/GetStarted.tsx
@@ -4,9 +4,9 @@ import { Button } from '../ui/button'
 
 const GetStarted = () => {
     return (
-        <div className='px-5 lg:px-32 py-56 bg-customBlue text-white text-center space-y-10 h-[80vh]'>
+        <div className='px-5 lg:px-32 py-32 lg:py-56 bg-customBlue text-white text-center space-y-10 h-[100vh] lg:h-[80vh]'>
             <h2 className='text-6xl'>Ready to Get Started?</h2>
-            <p className='text-2xl w-2/3 mx-auto'>
+            <p className='text-xl lg:text-2xl lg:w-2/3 mx-auto'>
                 Don&apos;t wait—your car deserves the best. Whether it's a routine check or major repairs, we&apos;re here to help. Click below to book your service and get back on the road in no time!
             </p>
             <Button className='text-xl'>
